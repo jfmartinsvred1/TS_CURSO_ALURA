8329bf93c31aad874be7943c36bd324b03efd8a3
jfmartinsvred1 <jfmartinsvred@gmail.com>
2023-09-05 01:06:44 -0300

    Criando domInjector decorator

--- app/dist/js/decorators/dom-injector.js
+++ app/dist/js/decorators/dom-injector.js
@@ -1,2 +1,16 @@
 export function domInjector(seletor) {
+    return function (target, propertyKey) {
+        console.log(`${propertyKey}`);
+        let elemento;
+        const getter = function () {
+            if (!elemento) {
+                elemento = document.querySelector(seletor);
+                console.log(`${propertyKey}`);
+            }
+            return elemento;
+        };
+        Object.defineProperty(target, propertyKey, {
+            get: getter
+        });
+    };
 }
